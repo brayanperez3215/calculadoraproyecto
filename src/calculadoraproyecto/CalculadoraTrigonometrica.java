6f70073be1de933078e79aa8f7149fa8f713e9d4
jose luis lesmes <42072299+joselesmes@users.noreply.github.com>
2018-08-24 10:32:23 -0500

    agregar funciones trigonometricas

--- src/calculadoraproyecto/CalculadoraTrigonometrica.java
+++ src/calculadoraproyecto/CalculadoraTrigonometrica.java
@@ -8,10 +8,32 @@ package calculadoraproyecto;
 
 import static java.lang.Math.*;
 
-public class CalculadoraTrigonometrica {
+public class Trigonometrica extends Calculadora {
     
-    double seno(){
+    
+
+
+    void cambiar(){
+    operando1=(float)Math.toRadians(operando1);
+            
+            }    
+            
+    void seno(){
+         cambiar();
+        resultado=(float)Math.sin(operando1);
+      
         
     }
     
+     void coseno(){
+          cambiar();
+        resultado=(float)Math.cos(operando1);
+        
+    }
+     void tangente(){
+         cambiar();
+         resultado=(float)Math.tan(operando1);
+         
+     }
+ 
 }
